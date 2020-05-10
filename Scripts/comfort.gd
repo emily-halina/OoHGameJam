2d8d95e07b2d03aba473946c8ed3e5f5b7b7d41e
Xulxuvar <teridax1306@gmail.com>
2020-05-10 15:52:41 -0500

    zone expansion

--- Scripts/comfort.gd
+++ Scripts/comfort.gd
@@ -10,18 +10,20 @@ var shape
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tint_color = Color(1,0,0,0.25)
+
+	shape = get_node("Area2D/CollisionShape2D").get_shape()
 	updateRadius()
 	pass # Replace with function body.
 
 func _draw():
-	print("radius is ", get_node("Area2D/CollisionShape2D").shape.radius)
-	draw_circle(Vector2(0,0),get_node("Area2D/CollisionShape2D").shape.radius, tint_color)
+	draw_circle(Vector2(0,0),shape.get_radius(), tint_color)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 func updateRadius():
-	get_node("Area2D/CollisionShape2D").shape.radius = gv.comfort_radius
+	shape.set_radius(gv.comfort_radius)
+	update()
 	print("radius is ", get_node("Area2D/CollisionShape2D").shape.radius)
 
 func _on_Area2D_body_entered(body):
